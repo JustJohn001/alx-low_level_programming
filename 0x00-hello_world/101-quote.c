b1f8fe8c8e2717b5f6f83f912fb83adca46b9297
JustJohn001 <john.ndungu@gmail.com>
2023-06-15 07:33:14 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to the standard error
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
