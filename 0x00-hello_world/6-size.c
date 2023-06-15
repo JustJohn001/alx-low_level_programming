cfc8a45a79e71e67c265c71aee3823631f7981ce
JustJohn001 <john.ndungu@gmail.com>
2023-06-15 06:50:19 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char is %zu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int is %zu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int is %zu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int is %zu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a char float %zu byte(s)\n", (unsigned long)sizeof(float));
+	return (0);
+}
+
