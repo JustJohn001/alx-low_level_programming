ad8914d58eae4cc002c98e4d73baa868035e3129
JustJohn001 <john.ndungu@gmail.com>
2023-06-15 06:56:47 -0700

    0x00-hello_world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
+ * main - Prints the sieze of various variable types
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char is %zu byte(s)", (unsigned long)sizeof(char));
-	printf("Size of an int is %zu byte(s)", (unsigned long)sizeof(int));
-	printf("Size of a long int is %zu byte(s)", (unsigned long)sizeof(long int));
-	printf("Size of a long long int is %zu byte(s)", (unsigned long)sizeof(long long int));
-	printf("Size of a char float %zu byte(s)", (unsigned long)sizeof(float));
+	printf("Size of a char is %zu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int is %zu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int is %zu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of a long long int is %zu byte(s)\n", (unsigned long)sizeof(long long int));
+	printf("Size of a char float %zu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
 
