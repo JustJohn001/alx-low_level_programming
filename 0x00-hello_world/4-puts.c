ccedd6d99249d284fb16c8d31dbfd424aae1c7ad
JustJohn001 <john.ndungu@gmail.com>
2023-06-15 05:52:37 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+
+/*A program that prints using puts()*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	
+	return(0);
+}
