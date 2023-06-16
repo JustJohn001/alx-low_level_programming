e165310ada729b700d0716f346373061954d17dc
JustJohn001 <john.ndungu@gmail.com>
2023-06-16 16:50:11 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,14 @@
 
 int main(void)
 {
-	int m, n;
+	int n;
 
-
-	for (m = 0; m < 10; m++)
+	for (n = 0; n < 10; n++)
 	{
-		for (n = 0; n < 10; n++)
-		{
-			putchar(m + '0');
 			putchar(n + '0');
-		
-			if (m !=9 || n !=9)
+			
+			if (n != 9)
 			putchar(',');
-			putchar(',');
-		}
 	}
 
 	return (0);
