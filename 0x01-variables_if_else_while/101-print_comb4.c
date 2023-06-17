bd6902e28ba14a48351d00afc0d37d1b0ea071f0
JustJohn001 <john.ndungu@gmail.com>
2023-06-16 17:14:54 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int m, n, o;
 
-	for (m = 0; m < 10; m++)
+	for (m = 0; m < 8; m++)
 	{
-		for (n = 0; n < 10; n++)
+		for (n = 0; n < 9; n++)
 		{
 			for (o = 0; o < 10; o++)
 			{
 				putchar(m + '0');
 				putchar(n + '0');
 				putchar(o + '0');
-				if (m !=9 || n != 9 || o != 9)
+				if (m != 7 || n != 8 || o != 9)
 				putchar(',');
 				putchar(' ');
 				putchar(' ');
