bd308296267e189d21b74c34c94dccbce675050b
JustJohn001 <john.ndungu@gmail.com>
2023-06-20 12:21:11 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: 1 for lowercase character 0 for otherwise
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+
+	return (0);
+}
