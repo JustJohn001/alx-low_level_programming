866edfc58602ee020b23b5cfffbbde5a13b7ecc2
JustJohn001 <john.ndungu@gmail.com>
2023-06-29 17:12:09 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@ void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < n--; i++)
 	{
 		j = a[i];
-		a[i] = a[n - 1 - 1];
-		a[n - 1 - i] = j;
+		a[i] = a[n];
+		a[n] = j;
 	}
 }
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: pointer to the string
+ * Return: pointer to the modified string
+ */
+char *leet(char *str)
+{
+	char *letters = "aeotlAEOTL";
+	char *leet_numbers = "4307143071";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leet_numbers[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
