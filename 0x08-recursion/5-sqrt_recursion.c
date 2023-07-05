84efeea9d3ca9845aadc322c5b87d7dab53f5fe3
JustJohn001 <john.ndungu@gmail.com>
2023-07-05 14:52:26 -0700

    0x08. C - Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,6 @@
 
 /**
  * _sqrt_check - checks the input number from n to the base
- * _sqrt_recursion - returns the natual square root of a number
  * @n: number to return the square root
  * @i: base number to check
  * Return: the square root, -1 if n does not have a natural square root
@@ -21,6 +20,12 @@ int _sqrt_check(int n, int i)
 
 	return (_sqrt_check(n + 1, i));
 }
+
+/**
+ * _sqrt_recursion - returns the natual square root of a number
+ * @n: number to check
+ * Return: the square root
+ */
 
 int _sqrt_recursion(int n)
 {
