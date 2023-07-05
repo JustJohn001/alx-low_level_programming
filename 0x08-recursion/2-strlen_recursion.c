983e56f5b68cc668f7c012d7738740cdca85ba9c
JustJohn001 <john.ndungu@gmail.com>
2023-07-05 13:40:39 -0700

    0x08. C - Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: pointer to string
- * Return: Length of the string 
+ * Return: Length of the string
  */
 
 int _strlen_recursion(char *s)
