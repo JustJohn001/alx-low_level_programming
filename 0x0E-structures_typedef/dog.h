0b3a801e8e8b6f7f24760254e7c26de2297366a5
JustJohn001 <john.ndungu@gmail.com>
2023-07-18 19:26:25 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG
 #define DOG
 
-struct dog {
+/**
+ * struct dog - contains a dog's information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
