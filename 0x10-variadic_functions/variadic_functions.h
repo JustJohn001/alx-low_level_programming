b2f0f74467afabaa41cae4288531202723c8acd3
JustJohn001 <john.ndungu@gmail.com>
2023-07-21 10:59:03 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,8 @@
+#ifndef HEADER
+#define HEADER
+
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -5,3 +10,13 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
+
+void print_char(va_list ap);
+
+void print_integer(va_list ap);
+
+void print_float(va_list ap);
+
+void print_string(va_list ap);
+
+#endif
